c874754904b5750ba0ff7f63065e3cda18310556
Dan Connolly <dckc@madmode.com>
2024-04-20 16:02:20 -0500

    SQUASHME: punt chokidar; use node's fs.watch

--- packages/file-watcher/src/FileWatchingService.js
+++ packages/file-watcher/src/FileWatchingService.js
@@ -1,62 +1,12 @@
 // @ts-check
-
-import EventEmitter from 'events';
-
-class AsyncEventEmitterIterator {
-  /**
-   * @param {ReturnType<typeof import('chokidar').watch>} emitter
-   */
-  constructor(emitter) {
-    this.emitter = emitter;
-    this.queue = []; // Queue events for asynchronous iteration
-  }
-
-  [Symbol.asyncIterator]() {
-    return this;
-  }
-
-  next() {
-    /** @type {Promise<IteratorResult<string>>} */
-    const p = new Promise((resolve, reject) => {
-      if (!this.emitter) {
-        return reject(new Error('Iterator is closed'));
-      }
-
-      /** @param {string} event */
-      const listener = event => {
-        this.queue.push(event); // Add event to the queue
-        resolve({ value: event, done: false }); // Resolve with event
-      };
-
-      this.emitter.on('event', listener); // Add event listener
-
-      // Resolve immediately if there's already an event in the queue
-      if (this.queue.length > 0) {
-        const event = this.queue.shift();
-        resolve({ value: event, done: false });
-        this.emitter.removeListener('event', listener); // Cleanup listener
-      }
-    });
-    return p;
-  }
-
-  return() {
-    if (this.emitter) {
-      this.emitter.removeAllListeners('event'); // Cleanup listeners
-    }
-    return Promise.resolve({ value: undefined, done: true });
-  }
-}
+import path from 'path';
 
 export class FileWatchingService {
   /**
-   * @param {Pick<typeof import('fs/promises'), 'readFile'>} fs
-   * @param {typeof import('chokidar').watch} watchFactory
+   * @param {Pick<typeof import('fs/promises'), 'readFile' | 'watch'>} fs
    */
-  constructor(fs, watchFactory) {
-    // Inject fs and watchFactory
+  constructor(fs) {
     this.fs = fs;
-    this.watch = watchFactory; // Store the watch function
   }
 
   /**
@@ -64,18 +14,17 @@ export class FileWatchingService {
    * @param {import('chokidar').WatchOptions} options
    */
   async *watchDirectory(directory, options = {}) {
-    const emitter = this.watch(directory, options);
-    const iterator = new AsyncEventEmitterIterator(emitter);
+    const events = this.fs.watch(directory, options);
 
-    // Close the emitter (optional)
-    // emitter.removeAllListeners();
-
-    for await (const path of iterator) {
-      if (path === undefined) continue;
+    for await (const event of events) {
+      console.log('@@', event);
+      const { filename } = event;
+      if (filename === null) break;
+      const fullPath = path.join(directory, filename);
       yield {
-        getName: () => path,
+        getName: () => filename,
         getContent: async () => {
-          const content = await this.fs.readFile(path, 'utf-8');
+          const content = await this.fs.readFile(fullPath, 'utf-8');
           return content;
         },
       };
